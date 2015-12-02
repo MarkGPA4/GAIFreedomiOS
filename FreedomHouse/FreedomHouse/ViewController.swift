fae7a4db6503768048d16d513fbb1b59539446d9
MarkLii <markli@bu.edu>
2015-12-01 20:21:55 -0500

    update viewcontroller

--- FreedomHouse/FreedomHouse/ViewController.swift
+++ FreedomHouse/FreedomHouse/ViewController.swift
@@ -8,15 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    //properties
+    @IBOutlet weak var username: UITextField!
+    
+    @IBOutlet weak var password: UITextField!
+    
+    @IBAction func login(sender: AnyObject) {
+        self.username.resignFirstResponder()
+        self.password.resignFirstResponder()
+    }
+    
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        return false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-
-        
-        
+        username.delegate = self
         
         NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillShow:"), name:UIKeyboardWillShowNotification, object: self.view.window);
         NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillHide:"), name:UIKeyboardWillHideNotification, object: nil);
@@ -31,7 +45,7 @@ class ViewController: UIViewController {
         let offset: CGSize = userInfo[UIKeyboardFrameBeginUserInfoKey]!.CGRectValue.size
         
         if keyboardSize.height == offset.height {
-            UIView.animateWithDuration(0.1, animations:{() -> Void in self.view.frame.origin.y -= keyboardSize.height
+            UIView.animateWithDuration(0.1, animations:{() -> Void in self.view.frame.origin.y -= keyboardSize.height 
             
             })
         } else {
@@ -42,7 +56,7 @@ class ViewController: UIViewController {
         
     
     
-    func overrideWillHide(sender: NSNotification) {
+    func KeyboardWillHide(sender: NSNotification) {
         let userInfo: [NSObject : AnyObject] = sender.userInfo!
         let keyboardSize: CGSize = userInfo[UIKeyboardFrameBeginUserInfoKey]!.CGRectValue.size
         self.view.frame.origin.y += keyboardSize.height
@@ -55,6 +69,15 @@ class ViewController: UIViewController {
 
     func DismissKeyBoard(){
         view.endEditing(true)
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        
+    }
+    
+    
+    func textFieldShouldEndEditing(textField: UITextField) -> Bool {  //delegate method
+    return false
     }
     
     
